8aedb4748ae4f20878429d04954366fe7a1209d1
Siarhei Lunski <lonnskai@gmail.com>
2022-03-14 20:39:42 +0100

    Cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "eslint-disable-next-line": "off",
+    "func-names": "off",
+    "import/no-import-module-exports": "off",
+    indent: ["error", 2],
+  },
+};
--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,42 +1,42 @@
-import axios from 'axios';
-
-const headers = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "Content-Type",
-  "Access-Control-Allow-Methods": "GET, POST, OPTION",
-}
+import axios from "axios";
 
 exports.handler = async function (event) {
-  const {
-    GITHUB_CLIENT_ID,
-    GITHUB_CLIENT_SECRET,
-    STATE_STRING,
-    REDIRECT_URI,
-  } = process.env;
-  const { code } = event?.queryStringParameters || {};
-  if (code) {
+  try {
+    const {
+      GITHUB_CLIENT_ID,
+      GITHUB_CLIENT_SECRET,
+      STATE_STRING,
+      REDIRECT_URI,
+    } = process.env;
+    const { code } = event?.queryStringParameters || {};
+    if (!code) throw new Error();
+
     const url = new URL("https://github.com/login/oauth/access_token");
     url.searchParams.append("client_id", GITHUB_CLIENT_ID);
     url.searchParams.append("client_secret", GITHUB_CLIENT_SECRET);
     url.searchParams.append("code", code);
     url.searchParams.append("state", STATE_STRING);
     url.searchParams.append("redirect_uri", REDIRECT_URI);
-    const response = await axios.get(url.href);
-    console.log(response);
-    const token = ''
 
+    const { data } = await axios.get(url.href);
+    if (!data) throw new Error();
+
+    const token = new URLSearchParams(data).get("access_token");
     return {
       statusCode: 200,
       body: JSON.stringify({
-        token
+        token,
       }),
-      headers
-    }
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, OPTION",
+      },
+    };
+  } catch (error) {
+    return {
+      statusCode: 401,
+      body: "Auth error",
+    };
   }
-
-  return {
-    statusCode: 401,
-    body: null,
-    headers
-  }
-}
+};
