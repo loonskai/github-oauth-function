b05a1499abddcfaee1ae5f258ee40d5a56f2440d
Siarhei Lunski <lonnskai@gmail.com>
2022-03-14 19:14:43 +0100

    Get token

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,20 +1,33 @@
-exports.handler = async function (event, context) {
-  const url = new URL("https://github.com/login/oauth/access_token");
-  // url.searchParams.append("client_id", GITHUB_CLIENT_ID);
-  // url.searchParams.append("client_secret", GITHUB_CLIENT_SECRET);
-  // url.searchParams.append("code", code);
-  // url.searchParams.append("state", STATE_STRING);
-  // url.searchParams.append("redirect_uri", REDIRECT_URI);
-  console.log(event)
-  console.log(process.env.GITHUB_CLIENT_ID)
-  console.log(process.env.GITHUB_CLIENT_SECRET)
-  console.log(process.env.STATE_STRING)
+import axios from 'axios';
 
-  const token = ''
+exports.handler = async function (event, context) {
+  const {
+    GITHUB_CLIENT_ID,
+    STATE_STRING,
+    REDIRECT_URI,
+  } = process.env;
+  const { code } = event?.queryStringParameters || {};
+  if (code) {
+    const url = new URL("https://github.com/login/oauth/access_token");
+    url.searchParams.append("client_id", GITHUB_CLIENT_ID);
+    url.searchParams.append("client_secret", GITHUB_CLIENT_SECRET);
+    url.searchParams.append("code", code);
+    url.searchParams.append("state", STATE_STRING);
+    url.searchParams.append("redirect_uri", REDIRECT_URI);
+    const response = await axios.get(url.href);
+    console.log(response);
+    const token = ''
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        token
+      })
+    }
+  }
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({
-      token
-    })
+    statusCode: 401,
+    body: null
   }
 }
